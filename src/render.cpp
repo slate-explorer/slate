88d219b464b9e5b5be86deb60759ba75383cbedd
JoshRuds <joshua.ruddick@icloud.com>
2026-08-10 21:09:04 +0100

    add openWindow() to render namespace

--- src/render.cpp
+++ src/render.cpp
@@ -9,7 +9,7 @@ struct WindowData {
 WindowData winDat;
 
 namespace { // private
-    LRESULT windProc(HWND window, UINT message, WPARAM wp, LPARAM lp) {
+    LRESULT windowTick(HWND window, UINT message, WPARAM wp, LPARAM lp) {
         LRESULT res = 0;
 
         switch (message) {
@@ -28,6 +28,30 @@ namespace { // private
 
 namespace render { // public
     void openWindow(const std::string name, int width, int height) {
+        WNDCLASS wc = {sizeof(WNDCLASS)};
         
+        wc.hCursor = LoadCursor(0, IDC_ARROW); // the cursor
+        wc.hInstance = GetModuleHandle(0); // current program instance
+        wc.lpszClassName = "SlateClass"; // class name
+        wc.style = CS_HREDRAW | CS_VREDRAW; // refresh on resize
+        wc.lpfnWndProc = windowTick; // call back function
+
+        if (!RegisterClass(&wc))
+            return;
+
+        HWND window = CreateWindowA(
+            wc.lpszClassName, // name of group
+            "Slate", // title
+            WS_OVERLAPPEDWINDOW | // normal mode
+            WS_VISIBLE, // ensure vibility
+            CW_USEDEFAULT, // pos x
+            CW_USEDEFAULT, // pos y
+            CW_USEDEFAULT, // size x
+            CW_USEDEFAULT, // size y
+            0, // no parent
+            0, // no menu
+            GetModuleHandle(0), // current program instance
+            0
+        );
     }
-}
+} 
